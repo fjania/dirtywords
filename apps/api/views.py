809789746d2042bf34c1d6667b85e1727a0e1db5
Frank Jania <fjania@findings.com>
2015-04-02 16:30:42 -0400

    how it works and promo

--- apps/api/views.py
+++ apps/api/views.py
@@ -3,6 +3,8 @@ import random
 from flask import jsonify
 
 from apps import app
+from apps.realwords import all_real_words
+
 from apps.markov import MarkovWords
 m = MarkovWords(3)
 
@@ -10,3 +12,7 @@ m = MarkovWords(3)
 def index(words):
     output = m.create_board(words, random.randint(3,10))
     return jsonify(board=output)
+
+@app.route('/api/realwords')
+def realwords():
+    return jsonify(words=list(all_real_words))
--- apps/game/views.py
+++ apps/game/views.py
@@ -6,3 +6,11 @@ from flask import render_template
 @app.route('/game')
 def game():
     return render_template('game.html')
+
+@app.route('/howitworks')
+def howitworks():
+    return render_template('howitworks.html')
+
+@app.route('/promo')
+def promo():
+    return render_template('promo.html')
